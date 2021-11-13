b8f87435410dad5f1ef1dc34f04aa02d32efe7bd
pradyumna <pradyumnaramesh26@gmail.com>
2021-11-13 18:11:45 +0530

    set up app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -18,7 +18,7 @@ const messageTwo = document.querySelector("#message-2");
 weatherForm.addEventListener("submit", (e) => {
   e.preventDefault();
   const location = search.value;
-  const url = `http://localhost:3000/weather?address=${location}`;
+  const url = `/weather?address=${location}`;
   messageOne.textContent = "loading...";
   messageTwo.textContent = "";
   fetch(url).then((response) => {
